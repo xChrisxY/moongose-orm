c0173a0d389844ab4f921d43a1c5b0e87c07c6e9
Christopher Yahir <223209@ids.upchiapas.edu.mx>
2023-10-25 19:57:36 -0600

    Finalización de las actualizaciones de comentarios y publicaciones

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const eschemaComentarios = require('../models/comment')
+const CommentSchema = require('../models/comment')
 const router = express.Router();
 
 // Método para agregar un comentario
 router.post('/comentario', (req, res) => {
-    const commentarios = eschemaComentarios(req.body);
+    const commentarios = CommentSchema(req.body);
     commentarios
         .save()
         .then((data) => res.json(data))
@@ -12,11 +12,42 @@ router.post('/comentario', (req, res) => {
 })
 
 router.get('/comentario', (req, res) => {
-    eschemaComentarios
+    CommentSchema
         .find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }))
 })
+
+router.put('/update-comment/:id', async (req, res) => {
+
+    try {
+        const idComment = req.params.id;
+        const newContent = req.body.contenido;
+
+        const updateComment = await CommentSchema.findByIdAndUpdate(
+            idComment,
+            { contenido: newContent },
+            { new: true }
+        );
+
+        if (!idComment) {
+
+            return res.status(404).json({ message: "Comment not found" })
+
+        }
+
+        console.log(`Comment update succesfully: ${updateComment}`);
+        return res.status(200).json(`Comment update succesfully`);
+
+    } catch (error) {
+
+        console.log(`Error al actualizar el usuario: ${error}`);
+        return res.status(500).json({ message: 'Error al actualizar la publicación'});
+
+    }
+
+});
+
 
 
 module.exports=router;
--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const PostSchema = require('../models/post')
+const PublicationSchema = require('../models/post')
 const router = express.Router();
 
 // Método para agregar una publicación
 router.post('/post', (req, res) => {
-    const publicacion = PostSchema(req.body);
+    const publicacion = PublicationSchema(req.body);
     publicacion
         .save()
         .then((data) => res.json(data))
@@ -12,11 +12,41 @@ router.post('/post', (req, res) => {
 })
 
 router.get('/post', (req, res) => {
-    PostSchema
+    PublicationSchema
         .find({})
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }))
 })
 
+router.put('/update-publication/:id', async (req, res) => {
 
-module.exports=router;
+    try {
+        const idPublication = req.params.id;
+        const newContent = req.body.contenido;
+
+        const updateUser = await PublicationSchema.findByIdAndUpdate(
+            idPublication,
+            { contenido: newContent },
+            { new: true }
+        );
+
+        if (!idPublication) {
+
+            return res.status(404).json({ message: "Publication not found" })
+
+        }
+
+        console.log(`Publication update succesfully: ${updateUser}`);
+        return res.status(200).json(`Publication update succesfully`);
+
+    } catch (error) {
+
+        console.log(`Error al actualizar el usuario: ${error}`);
+        return res.status(500).json({ message: 'Error al actualizar la publicación'});
+
+    }
+
+});
+
+
+module.exports = router;
